cb96025e0e9269dd7ff5fa19e8c48170095ed9f1
USER <USER@MYCOMPUTER>
2024-08-09 06:32:50 +0900

    Add project files.

---
+++ StudentPortal/Controllers/StudentsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using StudentPortal.Data;
+using StudentPortal.Models;
+using StudentPortal.Models.Entities;
+
+namespace StudentPortal.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public StudentsController(ApplicationDbContext DbContext)
+        {
+            dbContext = DbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed
+            };
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var student = await dbContext.Students.ToListAsync();
+            return View(student);
+        }
+    }
+}
